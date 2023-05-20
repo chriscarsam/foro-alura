25ee96b69d8104eb7e1f1fcdef242688b9f1b32d
Christian <chriscar.sam@gmail.com>
2023-05-20 13:57:59 -0500

    Spring Boot 3 + Swagger: Documentando una API REST

--- src/main/java/org/sam/foro/api/ApiApplication.java
+++ src/main/java/org/sam/foro/api/ApiApplication.java
@@ -1,13 +1,29 @@
 package org.sam.foro.api;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenApi() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("ChrisrianSam - Spring Boot 3")
+						.version("0.0.1")
+						.description(" API FORO ALURA")
+						.termsOfService("http://swagger.io/terms/")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org"))
+				);
 	}
 
 }
