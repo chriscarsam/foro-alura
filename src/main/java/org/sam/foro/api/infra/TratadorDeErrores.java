b3efc1d9c78dab8d126d3a9521fb9c9c1e3f4a89
Christian <chriscar.sam@gmail.com>
2023-05-21 10:26:24 -0500

    Tratando el error 404

---
+++ src/main/java/org/sam/foro/api/infra/TratadorDeErrores.java
@@ -0,0 +1,16 @@
+package org.sam.foro.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+}
