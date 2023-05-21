7f8be4caa6384a621d8b01d6db78693c8c906290
Christian <chriscar.sam@gmail.com>
2023-05-21 10:44:42 -0500

    Tratando el error 400

--- src/main/java/org/sam/foro/api/infra/TratadorDeErrores.java
+++ src/main/java/org/sam/foro/api/infra/TratadorDeErrores.java
@@ -2,6 +2,8 @@ package org.sam.foro.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,4 +15,16 @@ public class TratadorDeErrores {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException e){
+        var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    private record DatosErrorValidacion(String campo, String error){
+        public DatosErrorValidacion(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
 }
