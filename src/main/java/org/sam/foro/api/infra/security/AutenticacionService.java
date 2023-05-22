364ee4db579debc90f7c80440b7ab744167901dc
Christian <chriscar.sam@gmail.com>
2023-05-22 11:49:14 -0500

    Configuracion de seguridad #2

--- src/main/java/org/sam/foro/api/infra/security/AutenticacionService.java
+++ src/main/java/org/sam/foro/api/infra/security/AutenticacionService.java
@@ -15,6 +15,6 @@ public class AutenticacionService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return usuarioRepository.findByUsername(username);
+        return usuarioRepository.findByEmail(username);
     }
 }
