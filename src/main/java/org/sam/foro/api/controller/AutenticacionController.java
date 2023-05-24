a009cb0c32aa993b4b52e6f5b675be28d428a2b1
Christian <chriscar.sam@gmail.com>
2023-05-23 19:53:48 -0500

    Retornando usuarios mysql #2

--- src/main/java/org/sam/foro/api/controller/AutenticacionController.java
+++ src/main/java/org/sam/foro/api/controller/AutenticacionController.java
@@ -1,11 +1,13 @@
 package org.sam.foro.api.controller;
 
+import jakarta.validation.Valid;
 import org.sam.foro.api.domain.usuario.DatosAutenticacionUsuario;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +22,7 @@ public class AutenticacionController {
     }
 
     @PostMapping
-    public ResponseEntity autenticarUsuario(DatosAutenticacionUsuario datosAutenticacionUsuario){
+    public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
         Authentication token = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.email(),
                 datosAutenticacionUsuario.password());
         authenticationManager.authenticate(token);
