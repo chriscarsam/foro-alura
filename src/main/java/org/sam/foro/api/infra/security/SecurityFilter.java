777f2b2cd1fde84621773ed254b680006af13899
Christian <chriscar.sam@gmail.com>
2023-05-25 17:20:31 -0500

    Obteniendo el Token #1

--- src/main/java/org/sam/foro/api/infra/security/SecurityFilter.java
+++ src/main/java/org/sam/foro/api/infra/security/SecurityFilter.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = request.getHeader("Authorization").replace("Bearer ", "");
+        System.out.println(token);
         filterChain.doFilter(request, response);
     }
 }
